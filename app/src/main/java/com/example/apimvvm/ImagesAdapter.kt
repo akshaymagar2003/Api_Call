253ca25d5d997ab43ca8766f8ddf1521e5f2b474
Akshay@2003 <akshay88magar@gmail.com>
2023-06-01 01:02:43 +0530

    Image Adapter Updated

--- app/src/main/java/com/example/apimvvm/ImagesAdapter.kt
+++ app/src/main/java/com/example/apimvvm/ImagesAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.apimvvm
 
-class ImagesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+
+class ImagesAdapter : RecyclerView.Adapter<ImagesAdapter.ViewHolder>() {
+
+    private var images: List<String> = emptyList()
+
+    fun setImages(images: List<String>) {
+        this.images = images
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageUrl = images[position]
+        val imageView: ImageView = holder.itemView.findViewById(R.id.imageView)
+
+        Picasso.get().load(imageUrl).into(imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
